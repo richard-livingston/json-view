3dabeee58d2d00d6bab2237f6a5b4f05d8454f39
Richard Livingston <Richard Livingston>
2016-10-02 19:34:40 +0100

    Example

--- example/build.sh
+++
@@ -1,5 +0,0 @@
-rm -R ./build/
-mkdir ./build/
-cp ./index.html ./build/index.html
-cp ./node_modules/json-view/devtools.css ./build/devtools.css
-./node_modules/.bin/browserify -d -e ./index.js -o ./build/index.js
