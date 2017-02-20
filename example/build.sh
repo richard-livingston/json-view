ba03ed0ada7b6b5e7dc05bf6e7cd0d64ce6bea8e
Richard Livingston <Richard Livingston>
2017-02-20 20:27:02 +0000

    Do not wrap lines

--- example/build.sh
+++ example/build.sh
@@ -2,5 +2,5 @@ rm -R ./build/
 mkdir ./build/
 cp ./index.html ./build/index.html
 cp ./node_modules/json-view/devtools.css ./build/devtools.css
-npm i json-view
+npm i json-view@../
 ./node_modules/.bin/browserify -d -e ./index.js -o ./build/index.js
