67e7e3413a1cd1f63db8c64a083fddf40510ffcc
luyuan <luyuan.china@gmail.com>
2019-07-20 07:06:26 +0800

    Update example.

--- example/index.js
+++ example/index.js
@@ -17,31 +17,31 @@ var view = new JSONTreeView('example', {
 
 
 view.expand(true);
-view.withRootName = false;
+view.withRootName = true;
 
-view.on('change', function(self, key, oldValue, newValue){
-    console.log('change', key, oldValue, '=>', newValue, self);
+view.on('change', function(self, keyPath, oldValue, newValue){
+    console.log('change', keyPath, oldValue, '=>', newValue);
 });
-view.on('rename', function(self, key, oldName, newName) {
-    console.log('rename', key, oldName, '=>', newName, self);
+view.on('rename', function (self, keyPath, oldName, newName) {
+    console.log('rename', keyPath, oldName, '=>', newName);
 });
-view.on('delete', function(self, key, value, parentType) {
-    console.log('delete', key, '=', value, parentType, self);
+view.on('delete', function (self, keyPath, value, parentType) {
+    console.log('delete', keyPath, '=>', value, parentType);
 });
-view.on('append', function(self, key, nameOrValue, newValue) {
-    console.log('append', key, nameOrValue, '=>', newValue, self);
+view.on('append', function (self, keyPath, nameOrValue, newValue) {
+    console.log('append', keyPath, nameOrValue, '=>', newValue);
 });
-view.on('click', function(self, key, value) {
-    console.log('click', key, '=', value, self);
+view.on('click', function (self, keyPath, value) {
+    console.log('click', keyPath, '=>', value);
 });
-view.on('expand', function(self, key, value) {
-    console.log('expand', key, '=', value, self);
+view.on('expand', function (self, keyPath, value) {
+    console.log('expand', keyPath, '=>', value);
 });
-view.on('collapse', function(self, key, value) {
-    console.log('collapse', key, '=', value, self);
+view.on('collapse', function (self, keyPath, value) {
+    console.log('collapse', keyPath, '=>', value);
 });
-view.on('refresh', function(self, key, value) {
-    console.log('refresh', key, '=', value, self);
+view.on('refresh', function (self, keyPath, value) {
+    console.log('refresh', keyPath, '=>', value);
 });
 
 document.body.appendChild(view.dom);
@@ -78,4 +78,7 @@ document.getElementById('ro').addEventListener('change', function() {
 });
 document.getElementById('sc').addEventListener('change', function () {
     view.showCountOfObjectOrArray = !!this.checked;
+});
+document.getElementById('wr').addEventListener('change', function () {
+    view.withRootName = !!this.checked;
 });
